c80c2c9c71a100855982069958411462a1d234cb
stsnx <69145525+stsnx@users.noreply.github.com>
2022-10-19 19:24:39 +0700

    lab creational

---
+++ creational/src/CSVBookMetadataExporter.java
@@ -0,0 +1,11 @@
+public class CSVBookMetadataExporter extends BookMetadataExporter{
+    @Override
+    public BookMetadataFormatter make(){
+        try{
+            return new CSVBookMetadataFormatter();
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+}
